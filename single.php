aecae80647ca4febc17259fb03d89665ee97c83d
Loïc Herr <loic@herrloc.fr>
2017-03-02 14:58:58 +0100

    Couleur single article en fonction de la cat

--- single.php
+++ single.php
@@ -1,48 +1,53 @@
 <?php get_header(); ?>
+   
+   	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+   	
+   	<?php $cat = get_the_category(); ?>
+   	
+   	<article class="wrapper-single-article <?php echo $cat[0]->slug; ?>">
+    
+    <div class="featured-post" style="background-image: url('<?php the_post_thumbnail_url();?>');">
+      
+       <div class="wrapper">
+            <p class="h1-like featured-title"><?php the_title();?></p>
+            <div class="article-author-top">
+                <div class="image-circle">
+                    <?php echo get_avatar(get_the_author_meta('user_email')); ?>
+                </div>
+                <div class="author-text">
+                    <span class="author-name"><?php the_author_posts_link(); ?></span>
+                    <span class="author-title">Chef du monde</span>
+                    <div class="meta"><?php the_time('j F Y');?></div>
+                    
+                </div>
+            </div>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+       </div>
+    </div>
+    
+    
+    
+    <div class="wrapper grid has-gutter-xl single-article">
+        <div class="wrapper-article">
+            <?php the_content(); ?>
+        </div>
+        <div class="sidebar-article">
+            coucou
+        </div>
+    </div>
+    
+    
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
 
-			<?php the_content(); // Dynamic Content ?>
+			
 
 			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
 
 	<?php endwhile; ?>
 
@@ -57,11 +62,7 @@
 		<!-- /article -->
 
 	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	
+	</article>
 
 <?php get_footer(); ?>
