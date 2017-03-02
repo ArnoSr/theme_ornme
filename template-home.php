c890ea7fb18aca3773ed0826bcfb1ef352d14aa7
Loïc Herr <loic@herrloc.fr>
2017-03-02 11:00:36 +0100

    Home : featured article

--- template-home.php
+++ template-home.php
@@ -1,4 +1,41 @@
 <?php /* Template Name: Home template */ get_header(); ?>
+    
+    <?php
+
+    $argFeaturedPost = array(
+        'post_type'		=> 'post',
+        'posts_per_page' => 1,
+    ); 
+
+    $featuredPost = new WP_Query( $argFeaturedPost );
+
+    ?>
+
+    <?php if( $featuredPost->have_posts() ): ?>
+    <?php while( $featuredPost->have_posts() ) : $featuredPost->the_post(); ?>
+    
+    <div class="featured-post" style="background-image: url('<?php the_post_thumbnail_url();?>');">
+      
+       <div class="wrapper">
+          
+          <div class="w50">
+            <p class="h2-like featured-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></p>
+              <p class="featured-excerpt"><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
+               
+               <?php $cat = get_the_category(); ?>
+
+               <div class="meta">
+                   <p><span>par <?php the_author_link(); ?></span><span><?php the_time('j F Y');?></span><span class="featured-category name-category <?php echo $cat[0]->slug; ?>"><?php the_category(' ');?></span></p>
+               </div>
+          </div>
+
+       </div>
+    </div>
+    
+    <?php endwhile; ?>
+    <?php endif;?>
+    
+    <?php wp_reset_query();?>
 
 	<main role="main">
 		<!-- section -->
