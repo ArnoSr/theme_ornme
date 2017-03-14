32ecb8cfaea25ec628ca080a325e36b5e230c8b9
Loïc Herr <loic@herrloc.fr>
2017-03-14 15:22:39 +0100

    Page auteur

--- author.php
+++ author.php
@@ -1,77 +1,48 @@
 <?php get_header(); ?>
+    
+    <div class="tag-background">  
 
-	<main role="main">
-		<!-- section -->
-		<section>
+    <?php if(have_posts() ): ?>
+    
+    <div class="wrapper wrapper-plus home-latests-articles">
+        
+        <div class="hgroup-cat">
+            <h1>Auteur</h1>
+            <p><?php echo get_the_author() ; ?></p>          
+        </div>    
 
-		<?php if (have_posts()): the_post(); ?>
+        <div class="wrapper-articles">
 
-			<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+        <?php while(have_posts() ) : the_post(); ?>
 
-		<?php if ( get_the_author_meta('description')) : ?>
+            <div class="format format-<?php the_field('format_souhaite');?>">
 
-		<?php echo get_avatar(get_the_author_meta('user_email')); ?>
+                <a href="<?php the_permalink();?>" class="thumb"><?php the_post_thumbnail('large1000'); ?></a>
+                    <div class="wrap-content-article">
+                    <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                    <div class="excerpt">
+                        <?php the_excerpt();?>
+                    </div>
+                    <div class="meta">
+                     <div><span><svg viewBox="0 0 100 100" width="25" height="25"><use xlink:href="#icon-clock"></use></svg> <?php or_temps_lecture(get_the_content());?></span> <span>Publié le <?php the_time('j F Y');?></span></div>
+                     <div class="color-line"><span class="featured-author">par <?php the_author_posts_link(); ?></span> <span class="meta-cat"><?php the_category(' ');?></span></div>
+                    </div>
+                </div>
+            </div>
 
-			<h2><?php _e( 'About ', 'html5blank' ); echo get_the_author() ; ?></h2>
+        <?php endwhile; ?>
 
-			<?php echo wpautop( get_the_author_meta('description') ); ?>
+        </div>
+        
+        <?php get_template_part('pagination'); ?>
+    
+    </div>
+    <?php endif;?> 
+    
+</div>   
 
-		<?php endif; ?>
-
-		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-					</a>
-				<?php endif; ?>
-				<!-- /post thumbnail -->
-
-				<!-- post title -->
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<!-- /Post title -->
-
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
-
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+    
+    <?php include('vlog.php');?>
 
 <?php get_footer(); ?>
+
