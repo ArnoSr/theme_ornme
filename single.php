651b6c268fca147e5f0997be3904ce68f70352a7
Loïc Herr <loic@herrloc.fr>
2017-03-27 15:12:04 +0200

    Modifs social

--- single.php
+++ single.php
@@ -34,11 +34,8 @@
     
     <div class="wrapper single-article">
         <div class="wrapper-article">
-                       <div class="social">
-                <?php 
-                    echo do_shortcode('[juiz_sps buttons="facebook, twitter, linkedin"]');
-                ?>
-            </div>
+            
+            <?php include('snippets/social.php'); ?>
             <?php the_content(); ?>
         </div>
         <aside class="sidebar-article">
---
+++ snippets/social.php
@@ -0,0 +1,6 @@
+<div class="social">
+   <span class="partager-titre">Partager</span>
+    <?php 
+        echo do_shortcode('[juiz_sps buttons="facebook, twitter, linkedin"]');
+    ?>
+</div>
