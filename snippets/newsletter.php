91f4a854ffa641a00780379ef241807baea7bb21
Loïc Herr <loic@herrloc.fr>
2017-04-04 11:22:03 +0200

    Lien Mailjet

--- snippets/newsletter.php
+++ snippets/newsletter.php
@@ -3,10 +3,13 @@
         <h2>Or Norme : <span>tous les jours.</span></h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
        
-        <form action="#">
-            <input type="email" placeholder="Entrez votre email">
-            <input type="submit" value="S'inscrire">
-        </form>
+        <form id="form" action="https://app.mailjet.com/widget/iframe/1Ihf/2RI#" name="form" method="post">
+
+            <input id="w-preview-fields-content-cell-field-email" placeholder="Entrez votre email" name="w-field-field-11018-61576-408533-email" required="required" type="email">
+
+            <input id="submit_button" type="submit" value="S'inscrire"/>
+            
+       </form>   
            
     </div>
 </div>
