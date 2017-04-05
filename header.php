55834d3c0e02e0a52927d9d1b5b963f0e2001edc
Loïc Herr <loic@herrloc.fr>
2017-04-05 09:43:32 +0200

    Favicon

--- header.php
+++ header.php
@@ -9,6 +9,15 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
+		
+		
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="<?php echo get_template_directory_uri();?>/favicon/manifest.json">
+        <link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#000">
+
 
 	</head>
 	<body <?php body_class(); ?>>
