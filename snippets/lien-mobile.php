0bbbfdff69edd814cf8fc2afa102dd99539a8675
Loïc Herr <loic@herrloc.fr>
2017-04-06 10:22:43 +0200

    Lien android

--- snippets/lien-mobile.php
+++ snippets/lien-mobile.php
@@ -4,10 +4,8 @@
             <h2>Tout Or Norme <span>format poche</span></h2>
             <p>Restez connectés avec l’Art, la Culture et les projets Or Norme.</p>
             <ul class="list-mobile">
-                <li><a href="https://itunes.apple.com/us/app/or-norme/id1220468335?l=fr&ls=1&mt=8"><img src="<?php echo get_template_directory_uri();?>/img/BTN_AppStore.png" alt="Télécharger dans l'AppStore"></a></li>
-                <li><img src="<?php echo get_template_directory_uri();?>/img/BTN_GooglePlay.png" alt="Disponible sur Google Play"><div>
-                   <em style="font-size:1.4rem; text-align:center;">Bientôt disponible</em> 
-                </div></li>
+                <li><a href="https://itunes.apple.com/us/app/or-norme/id1220468335?l=fr&ls=1&mt=8" target="_blank"><img src="<?php echo get_template_directory_uri();?>/img/BTN_AppStore.png" alt="Télécharger dans l'AppStore"></a></li>
+                <li><a href="https://play.google.com/store/apps/details?id=com.ornorme" target="_blank"><img src="<?php echo get_template_directory_uri();?>/img/BTN_GooglePlay.png" alt="Disponible sur Google Play"></a></li>
             </ul>
         </div>
         <div class="img-cross-mobile">
