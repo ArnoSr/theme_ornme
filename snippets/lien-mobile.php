c0fb9270d76b10f6c27543ad0b50c36a15ad48b0
Loïc Herr <loic@herrloc.fr>
2017-04-06 09:10:56 +0200

    Modifs

--- snippets/lien-mobile.php
+++ snippets/lien-mobile.php
@@ -2,10 +2,12 @@
    <div class="wrapper">
         <div class="mobile-text">
             <h2>Tout Or Norme <span>format poche</span></h2>
-            <p>Restez connecter avec l’Art, la Culture et les projets Or Norme.</p>
+            <p>Restez connectés avec l’Art, la Culture et les projets Or Norme.</p>
             <ul class="list-mobile">
-                <li><a href="#"><img src="<?php echo get_template_directory_uri();?>/img/BTN_AppStore.png" alt="Télécharger dans l'AppStore"></a></li>
-                <li><a href="#"><img src="<?php echo get_template_directory_uri();?>/img/BTN_GooglePlay.png" alt="Disponible sur Google Play"></a></li>
+                <li><a href="https://itunes.apple.com/us/app/or-norme/id1220468335?l=fr&ls=1&mt=8"><img src="<?php echo get_template_directory_uri();?>/img/BTN_AppStore.png" alt="Télécharger dans l'AppStore"></a></li>
+                <li><img src="<?php echo get_template_directory_uri();?>/img/BTN_GooglePlay.png" alt="Disponible sur Google Play"><div>
+                   <em style="font-size:1.4rem; text-align:center;">Bientôt disponible</em> 
+                </div></li>
             </ul>
         </div>
         <div class="img-cross-mobile">
